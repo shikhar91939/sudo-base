a8965785c6d87291f42952643c8471339ddfc546
Shikhar <shikhar.91939@gmail.com>
2019-08-28 00:54:25 -0400

    login successful

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 const inquirer = require('inquirer');
 const axois = require('axios');
+const querystring = require('querystring'); // move to 'only when needed'
 
-const realmQ = {
+const realmQues = {
     name: 'realm',
-    message: 'what is the realm you want to connect to?',
+    message: 'realm:',
 };
-const usernameQ = {
+const usernameQues = {
     name: 'username',
-    message: 'your username:',
+    message: 'username:',
 }
-const passQ = {
+const passQues = {
     type: 'password',
     name: 'password',
+    message: 'password:',
     mask: '',
 }
 
 module.exports = async () => {
-    console.log('Welcome to Quick Base. Please login to continue');
-    const creds = await inquirer.prompt([realmQ, usernameQ, passQ]);
-    login(creds);
+    console.log('To use Quick Base, please login with your realm, username and password');
+    const loginData = await inquirer.prompt([realmQues, usernameQues, passQues]);
+    login(loginData);
 }
-const login = (creds) => {
-    const url = 'https://'+creds.realm+'.quickbase.com/db/main';
+const login = (loginData) => {
+    const url = 'https://'+loginData.realm+'.quickbase.com/db/main';
     // const url = 'https://webhook.site/60fd4716-07cf-4ae6-b7d9-00865a46b782';
-    console.log(url);
-    
-    const auth = {
+    const headers = {
         'QUICKBASE-ACTION': 'API_Authenticate',
-        // 'Content-Type': 'application/xml',
-        username: creds.username,
-        password: creds.password,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/json',// application/xml
     }
-    axois.post(url, auth).then(onResponse = (response) => {
+    const credentials = {
+        username: loginData.username,
+        password: loginData.password,
+    }
+
+    axois({
+        method: 'post',
+        url: url,
+        headers: headers,
+        data: querystring.stringify(credentials),
+    })
+    .then(onResponse = (response) => {
         console.log(response);
     }).catch( (error) => {
-        console.log('error while login', error);
+        console.log('error in login', error);
     });
 }
